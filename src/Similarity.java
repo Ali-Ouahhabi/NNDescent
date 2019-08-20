6aede62ed60eee3ad2feade96c219dea150b5bf1
Ali-Ouahhabi <ali.ohhb@gmail.com>
2019-08-21 03:59:16 +0700

    Similarity Interface

---
+++ src/Similarity.java
@@ -0,0 +1,13 @@
+/*
+ * A\iOuahhabi.
+ */
+
+import java.util.List;
+
+/**
+ * @author Admin
+ */
+public interface Similarity {
+    //interface to reference the object with the similarity algorithm  
+    public Double similarityComuting(List<Double> p1, List<Double> p2);
+}
